5859de0063e4f3f5f4aa0fce3ff2e7c33e4d6b03
axceljalos <axcel.jalos@dorsu.edu.ph>
2026-01-09 14:06:30 +0800

    Exer_3C_Jalos

---
+++ Exer_3.C_JALOS/Shape.java
@@ -0,0 +1,24 @@
+public class Shape {
+    
+    private float area;
+
+    public Shape(){
+        this.area = 2.0f;
+    }
+
+    public float calculateArea(){
+        return 1.0f;
+    }
+
+    public String displayInfo(){
+        return null;
+    }
+    public float getArea(){
+        return this.area;
+    }
+
+    public String DisplayInfo() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'DisplayInfo'");
+    }
+}
